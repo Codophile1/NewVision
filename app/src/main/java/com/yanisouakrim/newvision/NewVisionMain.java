0a8f6b1c1ba236607a65f8e37a5823374d797415
Kibo4 <william.mocaer@gmail.com>
2017-03-22 15:01:33 +0100

    cc

--- app/src/main/java/com/yanisouakrim/newvision/NewVisionMain.java
+++ app/src/main/java/com/yanisouakrim/newvision/NewVisionMain.java
@@ -9,5 +9,6 @@ public class NewVisionMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_vision_main);
+        //hey
     }
 }
